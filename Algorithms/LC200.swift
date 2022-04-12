3106fffa7c43e31b80c9cb101164aa9c142b22ed
Jess Phillips <tripphillips5@gmail.com>
2022-04-12 09:23:43 -0500

    - update LC200 header comment

--- Algorithms/LC200.swift
+++ Algorithms/LC200.swift
@@ -4,9 +4,11 @@
 //
 //  Created by Trip Phillips on 4/11/22.
 //
+
+// 200. Number of Islands
+//
 // Medium
 //
-// 200. Number of Islands
 // Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
 // You may assume all four edges of the grid are all surrounded by water.

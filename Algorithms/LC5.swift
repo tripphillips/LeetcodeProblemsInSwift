09138b0326b59bfd8be5cecd14d5efbfa22f3f8d
Jess Phillips <tripphillips5@gmail.com>
2022-04-13 09:48:36 -0500

    update LC5

--- Algorithms/LC5.swift
+++ Algorithms/LC5.swift
@@ -15,36 +15,36 @@ import Foundation
 
 class LC5Solution {
     func longestPalindrome(_ s: String) -> String {
-            guard s.count > 1 && !s.isEmpty else { return s }
+        guard s.count > 1 && !s.isEmpty else { return s }
+        
+        let charArray = Array(s)
+        var start = 0
+        var end = 0
+        
+        for i in 0..<charArray.count-1 {
             
-            let charArray = Array(s)
-            var start = 0
-            var end = 0
+            let oddLength = expandAroundCenter(charArray: charArray, left: i, right: i)
             
-            for i in 0..<charArray.count-1 {
-                
-                let oddLength = expandAroundCenter(charArray: charArray, left: i, right: i)
-                
-                let evenLength = expandAroundCenter(charArray: charArray, left: i, right: i+1)
-                let lenght = max(oddLength, evenLength)
-                
-                if (lenght > end - start) {
-                    start = i - ((lenght - 1) / 2)
-                    end = i + (lenght/2)
-                }
+            let evenLength = expandAroundCenter(charArray: charArray, left: i, right: i+1)
+            let lenght = max(oddLength, evenLength)
+            
+            if (lenght > end - start) {
+                start = i - ((lenght - 1) / 2)
+                end = i + (lenght/2)
             }
-            return String(charArray[start...(end)])
+        }
+        return String(charArray[start...(end)])
+    }
+    
+    func expandAroundCenter(charArray: [Character], left: Int, right: Int) -> Int {
+        var left = left
+        var right = right
+        
+        while (left >= 0 && right < charArray.count && charArray[left] == charArray[right]) {
+            left -= 1
+            right += 1
         }
         
-        func expandAroundCenter(charArray: [Character], left: Int, right: Int) -> Int {
-            var left = left
-            var right = right
-            
-            while (left >= 0 && right < charArray.count && charArray[left] == charArray[right]) {
-                left -= 1
-                right += 1
-            }
-            
-            return right - left - 1
-        }
+        return right - left - 1
+    }
 }
